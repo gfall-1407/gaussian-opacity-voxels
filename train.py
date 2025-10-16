da870c927b4ab3e661dd9fd24bea7446281f3e69
gunther <1092427203@qq.com>
2025-10-16 15:42:54 +0800

    train_debug.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from random import randint
 from utils.loss_utils import l1_loss, ssim
 from gaussian_renderer import render, network_gui
 import sys
-from scene import Scene, GaussianModel
+from scene import Scene, GovsModel
 from utils.general_utils import safe_state
 import uuid
 from tqdm import tqdm
@@ -34,12 +34,12 @@ import numpy as np
 def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoint_iterations, checkpoint, debug_from):
     first_iter = 0
     tb_writer = prepare_output_and_logger(dataset)
-    gaussians = GaussianModel(dataset.sh_degree)
-    scene = Scene(dataset, gaussians)
-    gaussians.training_setup(opt)
-    if checkpoint:
-        (model_params, first_iter) = torch.load(checkpoint)
-        gaussians.restore(model_params, opt)
+    govs = GovsModel(dataset.sh_degree)
+    scene = Scene(dataset, govs)
+    govs.training_setup(opt)
+    # if checkpoint:
+    #     (model_params, first_iter) = torch.load(checkpoint)
+    #     govs.restore(model_params, opt)
 
     bg_color = [1, 1, 1] if dataset.white_background else [0, 0, 0]
     background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
@@ -52,60 +52,39 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
     progress_bar = tqdm(range(first_iter, opt.iterations), desc="Training progress")
     first_iter += 1
     for iteration in range(first_iter, opt.iterations + 1):        
-        if network_gui.conn == None:
-            network_gui.try_connect()
-        while network_gui.conn != None:
-            try:
-                net_image_bytes = None
-                custom_cam, do_training, pipe.convert_SHs_python, pipe.compute_cov3D_python, keep_alive, scaling_modifer = network_gui.receive()
-                if custom_cam != None:
-                    net_image = render(custom_cam, gaussians, pipe, background, scaling_modifer)["render"]
-                    net_image_bytes = memoryview((torch.clamp(net_image, min=0, max=1.0) * 255).byte().permute(1, 2, 0).contiguous().cpu().numpy())
-                network_gui.send(net_image_bytes, dataset.source_path)
-                if do_training and ((iteration < int(opt.iterations)) or not keep_alive):
-                    break
-            except Exception as e:
-                network_gui.conn = None
+        # if network_gui.conn == None:
+        #     network_gui.try_connect()
+        # while network_gui.conn != None:
+        #     try:
+        #         net_image_bytes = None
+        #         custom_cam, do_training, pipe.convert_SHs_python, pipe.compute_cov3D_python, keep_alive, scaling_modifer = network_gui.receive()
+        #         if custom_cam != None:
+        #             net_image = render(custom_cam, govs, pipe, background, scaling_modifer)["render"]
+        #             net_image_bytes = memoryview((torch.clamp(net_image, min=0, max=1.0) * 255).byte().permute(1, 2, 0).contiguous().cpu().numpy())
+        #         network_gui.send(net_image_bytes, dataset.source_path)
+        #         if do_training and ((iteration < int(opt.iterations)) or not keep_alive):
+        #             break
+        #     except Exception as e:
+        #         network_gui.conn = None
 
         iter_start.record()
 
-        gaussians.update_learning_rate(iteration)
+        govs.update_learning_rate(iteration)
 
         # Every 1000 its we increase the levels of SH up to a maximum degree
         if iteration % 1000 == 0:
-            gaussians.oneupSHdegree()
+            govs.oneupSHdegree()
 
         # Pick a random Camera
         if not viewpoint_stack:
             viewpoint_stack = scene.getTrainCameras().copy()
         viewpoint_cam = viewpoint_stack.pop(randint(0, len(viewpoint_stack)-1))
-        # if iteration == 1:
-        #      with open('render_output/viewpoint.txt', 'w', encoding='utf-8') as viewpoint_f:
-        #           pass
-        # with open('render_output/viewpoint.txt', 'a', encoding='utf-8') as viewpoint_f:
-        #     viewpoint_f.write(str(iteration) + ": " + viewpoint_cam.image_name + "\n")
-        #     for row in viewpoint_cam.R:
-        #         line = ','.join(f'{x:.4f}' for x in row)
-        #         viewpoint_f.write(line)
-        #         viewpoint_f.write('\n')
-        #     T_py = viewpoint_cam.T.tolist()
-        #     T_save = str(T_py) 
-        #     viewpoint_f.write(T_save) 
-        #     viewpoint_f.write('\n')
-        #     viewpoint_f.write('-----------------------\n')
 
         # Render
         if (iteration - 1) == debug_from:
             pipe.debug = True
-        render_pkg = render(viewpoint_cam, gaussians, pipe, background)
+        render_pkg = render(viewpoint_cam, govs, pipe, background)
         image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
-        
-        # if ((iteration - 1) % 100 == 0 and iteration <= opt.densify_from_iter) or ((iteration - 1) % 5000 == 0 and iteration > opt.densify_from_iter):
-        #     image_np = image.detach().cpu().numpy()
-        #     image_np = np.transpose(image_np, (1, 2, 0))
-        #     array = np.array(image_np*255.0, dtype=np.byte)  
-        #     image_save = Image.fromarray(array, "RGB")  
-        #     image_save.save("render_output/output_" + str(iteration) + ".png" )
 
         # Loss
         gt_image = viewpoint_cam.original_image.cuda()
@@ -133,31 +112,24 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
             # Densification
             if iteration < opt.densify_until_iter:
                 # Keep track of max radii in image-space for pruning
-                gaussians.max_radii2D[visibility_filter] = torch.max(gaussians.max_radii2D[visibility_filter], radii[visibility_filter])
-                gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter)
+                govs.max_radii2D[visibility_filter] = torch.max(govs.max_radii2D[visibility_filter], radii[visibility_filter])
+                govs.add_densification_stats(viewspace_point_tensor, visibility_filter)
 
                 if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
                     size_threshold = 20 if iteration > opt.opacity_reset_interval else None
-                    gaussians.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold)
+                    govs.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold)
                 
                 if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
-                    gaussians.reset_opacity()
+                    govs.reset_opacity()
 
             # Optimizer step
             if iteration < opt.iterations:
-                gaussians.optimizer.step()
-                gaussians.optimizer.zero_grad(set_to_none = True)
+                govs.optimizer.step()
+                govs.optimizer.zero_grad(set_to_none = True)
 
             if (iteration in checkpoint_iterations):
                 print("\n[ITER {}] Saving Checkpoint".format(iteration))
-                torch.save((gaussians.capture(), iteration), scene.model_path + "/chkpnt" + str(iteration) + ".pth")
-
-        # if iteration == 1:
-        #     with open('render_output/point_count.txt', 'w', encoding='utf-8') as count_f:
-        #         pass
-        # with open('render_output/point_count.txt', 'a', encoding='utf-8') as count_f:
-        #     count_str = str(iteration) + " " + str(gaussians.get_xyz.shape[0]) + '\n'
-        #     count_f.write(count_str)
+                torch.save((govs.capture(), iteration), scene.model_path + "/chkpnt" + str(iteration) + ".pth")
 
 def prepare_output_and_logger(args):    
     if not args.model_path:
