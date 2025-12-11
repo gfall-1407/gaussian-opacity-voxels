4b7c673b70e4eff674b868f0407ac8747df2293e
gunther <1092427203@qq.com>
2025-12-11 22:56:01 +0800

    mesh

--- render.py
+++ render.py
@@ -21,18 +21,104 @@ from argparse import ArgumentParser
 from arguments import ModelParams, PipelineParams, get_combined_args
 from gaussian_renderer import GaussianModel
 
-def render_set(model_path, name, iteration, views, gaussians, pipeline, background):
+import open3d as o3d
+import numpy as np
+
+def to_cam_open3d(viewpoint_stack):
+    camera_traj = []
+    for i, viewpoint_cam in enumerate(viewpoint_stack):
+        W = viewpoint_cam.image_width
+        H = viewpoint_cam.image_height
+        ndc2pix = torch.tensor([
+            [W / 2, 0, 0, (W-1) / 2],
+            [0, H / 2, 0, (H-1) / 2],
+            [0, 0, 0, 1]]).float().cuda().T
+        intrins =  (viewpoint_cam.projection_matrix @ ndc2pix)[:3,:3].T
+        intrinsic=o3d.camera.PinholeCameraIntrinsic(
+            width=viewpoint_cam.image_width,
+            height=viewpoint_cam.image_height,
+            cx = intrins[0,2].item(),
+            cy = intrins[1,2].item(), 
+            fx = intrins[0,0].item(), 
+            fy = intrins[1,1].item()
+        )
+
+        extrinsic=np.asarray((viewpoint_cam.world_view_transform.T).cpu().numpy())
+        camera = o3d.camera.PinholeCameraParameters()
+        camera.extrinsic = extrinsic
+        camera.intrinsic = intrinsic
+        camera_traj.append(camera)
+
+    return camera_traj
+
+def focus_point_fn(poses: np.ndarray) -> np.ndarray:
+  """Calculate nearest point to all focal axes in poses."""
+  directions, origins = poses[:, :3, 2:3], poses[:, :3, 3:4]
+  m = np.eye(3) - directions * np.transpose(directions, [0, 2, 1])
+  mt_m = np.transpose(m, [0, 2, 1]) @ m
+  focus_pt = np.linalg.inv(mt_m.mean(0)) @ (mt_m @ origins).mean(0)[:, 0]
+  return focus_pt
+
+def render_set(model_path, name, iteration, views, gaussians, pipeline, background, mesh_name, depth_trunc, voxel_size, sdf_trunc, if_mesh = False):
     render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
     gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
 
     makedirs(render_path, exist_ok=True)
     makedirs(gts_path, exist_ok=True)
 
+    rgbmaps = []
+    depthmaps = []
+    viewpoint_stack = []
+
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
-        rendering = render(view, gaussians, pipeline, background)["render"]
+        rendering_pkg = render(view, gaussians, pipeline, background)["render"]
+        rendering = rendering_pkg[0:3, :, :]
+        depth = rendering_pkg[3:4, :, :]
+        rgbmaps.append(rendering.cpu())
+        depthmaps.append(depth.cpu())
+        viewpoint_stack.append(view)
         gt = view.original_image[0:3, :, :]
         torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
         torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+    
+    if if_mesh:
+        torch.cuda.empty_cache()
+        c2ws = np.array([np.linalg.inv(np.asarray((cam.world_view_transform.T).cpu().numpy())) for cam in viewpoint_stack])
+        poses = c2ws[:,:3,:] @ np.diag([1, -1, -1, 1])
+        center = (focus_point_fn(poses))
+        radius = np.linalg.norm(c2ws[:,:3,3] - center, axis=-1).min()
+        center = torch.from_numpy(center).float().cuda()
+
+        depth_trunc = (radius * 2.0) if depth_trunc < 0  else depth_trunc
+        voxel_size = (depth_trunc / 1024) if voxel_size < 0 else voxel_size
+        sdf_trunc = 5.0 * voxel_size if sdf_trunc < 0 else sdf_trunc
+        
+        volume = o3d.pipelines.integration.ScalableTSDFVolume(
+            voxel_length = voxel_size,
+            sdf_trunc = sdf_trunc,
+            color_type=o3d.pipelines.integration.TSDFVolumeColorType.RGB8
+        )
+
+        for i, cam_o3d in tqdm(enumerate(to_cam_open3d(viewpoint_stack)), desc="TSDF integration progress"):
+            rgb = rgbmaps[i]
+            depth = depthmaps[i]
+            
+            # if we have mask provided, use it
+            # if mask_backgrond and (viewpoint_stack[i].gt_alpha_mask is not None):
+            #     depth[(viewpoint_stack[i].gt_alpha_mask < 0.5)] = 0
+
+            # make open3d rgbd
+            rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+                o3d.geometry.Image(np.asarray(np.clip(rgb.permute(1,2,0).cpu().numpy(), 0.0, 1.0) * 255, order="C", dtype=np.uint8)),
+                o3d.geometry.Image(np.asarray(depth.permute(1,2,0).cpu().numpy(), order="C")),
+                depth_trunc = depth_trunc, convert_rgb_to_intensity=False,
+                depth_scale = 1.0
+            )
+
+            volume.integrate(rgbd, intrinsic=cam_o3d.intrinsic, extrinsic=cam_o3d.extrinsic)
+
+        mesh = volume.extract_triangle_mesh()
+        o3d.io.write_triangle_mesh(os.path.join(model_path, name), mesh)
 
 def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool):
     with torch.no_grad():
@@ -42,11 +128,16 @@ def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParam
         bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
         background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
+        name = 'fuse.ply'
+        depth_trunc = args.depth_trunc
+        voxel_size = args.voxel_size
+        sdf_trunc = args.sdf_trunc
+
         if not skip_train:
-             render_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background)
+             render_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background, name, depth_trunc, voxel_size, sdf_trunc, True)
 
         if not skip_test:
-             render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background)
+             render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background, name, depth_trunc, voxel_size, sdf_trunc)
 
 if __name__ == "__main__":
     # Set up command line argument parser
@@ -57,6 +148,9 @@ if __name__ == "__main__":
     parser.add_argument("--skip_train", action="store_true")
     parser.add_argument("--skip_test", action="store_true")
     parser.add_argument("--quiet", action="store_true")
+    parser.add_argument("--voxel_size", default=-1.0, type=float, help='Mesh: voxel size for TSDF')
+    parser.add_argument("--depth_trunc", default=-1.0, type=float, help='Mesh: Max depth range for TSDF')
+    parser.add_argument("--sdf_trunc", default=-1.0, type=float, help='Mesh: truncation value for TSDF')
     args = get_combined_args(parser)
     print("Rendering " + args.model_path)
 
